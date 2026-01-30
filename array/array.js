1e18add2a76a1f0fe8a9676325f6f46253ec0768
DevSohailPathan <pathansohal68@gmail.com>
2026-01-30 15:23:16 +0530

    Add files via upload

---
+++ array/array.js
@@ -0,0 +1,22 @@
+let array = [0, 1, 2]
+
+array[0] = 50
+array[1] = 60
+array[2] = 70
+
+
+
+array[3] = 80
+array[4] = 90
+
+// console.log(array)
+
+
+// using shiftfront function
+array.unshift(290)
+console.log("Using shift function")
+// console.log(array)
+
+array.push(237)
+console.log("Using push function")
+console.log(array)
---
+++ array/array2.js
@@ -0,0 +1,19 @@
+// let array = [ 10 ,20 , 30 ]
+
+// array [1]= 400 
+// array [2]= 500
+// array [3]= 600
+
+// array[4] = 500
+// array[5]=600
+
+// console.log(array);
+
+// array.push(300)
+// console.log("using push function")
+// console.log(array)
+
+// array.unshift(27)
+// console.log("Using shift function")
+// console.log(array)
+
