79c5c4457ca6b6b6943f1f6833a0ddf601d060d3
DevSohailPathan <pathansohal68@gmail.com>
2026-01-31 10:37:56 +0530

    Add files via upload

---
+++ Javascript/array/class-1.js
@@ -0,0 +1,13 @@
+// Create Array
+
+let numbers = [10, 20, 30, 40];
+
+let strings = ["string1", "string2"];
+let answers = [true, false];
+
+let array = [" string", 10];
+console.log(numbers);
+
+console.log(number[0]);
+console.log(number[0],number[1])
+console.log(typeof(array[1]))
---
+++ Javascript/array/class-2.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+console.log(array);
+
+array[3] = 80;
+console.log(array);
+
+array[4] = 100;
+console.log(array);
+
+
